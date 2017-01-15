3d93c3716b41aedf68c8a929b7ba2649b75d5a2c
Jacob Goense <dugo@xs4all.nl>
2017-01-15 06:03:51 -0500

    try

--- test/basic.js
+++ test/basic.js
@@ -30,7 +30,7 @@ describe("testing javascript in the browser", function() {
   it("should handle a click", function(done) {
     var terminal = this.browser.findElement(webdriver.By.id('tty0'));
     terminal.click();
-    window.scrollTo(0, 0);
+    this.browser.window.scrollTo(0, 0);
     terminal.getText().then(function(txt) {
       assert.equal(txt, "Terminal uses canvas");
       done();
