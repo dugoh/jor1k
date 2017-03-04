0648c36dee233c884cc4fdde3825b945554ecbb9
travis <dugo@xs4all.nl>
2017-03-04 22:48:33 +0000

    Deploy to GitHub Pages

---
+++ README.md
@@ -0,0 +1,47 @@
+# Fork of jor1k
+
+jor1k is a OpenRISC 1000 emulator written in JavaScript running Linux.
+
+Oldbsd is intended to become an emdaptation of
+* "Twenty Years of Berkeley Unix: From AT&T-Owned to Freely Redistributable"
+* "The Daemon, the GNU, and the Penguin"
+
+and other tales of UNIX/BSD lore running in simh in linux in jor1k in javascript in your browser.
+
+
+##emdaptation
+[em-d-uh p-tey-shuh n] 
+
+noun
+* 1.
+the state of being emdapted; running in an emulator.
+* 2.
+something produced by emulating:
+an emdaptation of a UNIX history book.
+
+Origins: Word play on emscripten, embiggen, emulating, adapting
+
+
+
+### Experiments and fun with CI and jor1k, mostly unfinished
+
+![jor1k](http://www.oldbsd.org/ss.png)
+
+* Automated installation of UNIX v6 on [oldbsd](http://www.oldbsd.org)
+* Flat [vt100] glass tty (http://www.hit2000.nl/jor1k/demos/crt2.html)
+* Bulged / pinched [ADM-3a] (http://www.hit2000.nl/jor1k/demos/crt.html)
+
+![jor1k](http://www.oldbsd.org/ss2.png)
+
+* Experiment of the [day] (http://www.minac.nl/l/jor1k/demos/2ttys.html) - 2 model 33 teletypes
+* This fork. Anything outside demos/2ttys.html will break.
+
+[![Build Status](https://travis-ci.org/dugoh/jor1k.svg?branch=master)](https://travis-ci.org/dugoh/jor1k)
+
+[![Sauce Test Status](https://saucelabs.com/buildstatus/dugoh)](https://saucelabs.com/u/dugoh)
+
+[![Sauce Test Status](https://saucelabs.com/browser-matrix/dugoh.svg)](https://saucelabs.com/u/dugoh)
+
+https://dugoh.github.io/jor1k/
+
+* An attempt to pipe this through Travis-CI and Saucelabs
---
+++ test/basic.js
@@ -0,0 +1,67 @@
+var assert = require("assert");
+var webdriver = require("selenium-webdriver");
+
+describe("testing javascript in the browser", function() {
+  beforeEach(function() {
+    if (process.env.SAUCE_USERNAME != undefined) {
+      this.browser = new webdriver.Builder()
+      .usingServer('http://'+ process.env.SAUCE_USERNAME+':'+process.env.SAUCE_ACCESS_KEY+'@ondemand.saucelabs.com:80/wd/hub')
+      .withCapabilities({
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        build: process.env.TRAVIS_BUILD_NUMBER,
+        username: process.env.SAUCE_USERNAME,
+        accessKey: process.env.SAUCE_ACCESS_KEY,
+        platform: "Windows 10",
+        browserName: "firefox",
+        version: "50.0"
+      }).build();
+    } else {
+      this.browser = new webdriver.Builder()
+      .withCapabilities({
+        browserName: "chrome"
+      }).build();
+    }
+
+    return this.browser.get("http://localhost:8000/index.html");
+  });
+
+  afterEach(function() {
+    return this.browser.quit();
+  });
+
+  it("should handle a click", function(done) {
+    this.browser.sleep(5000);
+    //var terminal = this.browser.findElement(webdriver.By.id('tty0'));
+    //this.browser.sleep(5000);
+    //terminal.click();
+    //this.browser.SwitchTo().ActiveElement().SendKeys("uname -a");
+    //terminal.click();
+    //this.browser.SwitchTo().ActiveElement().SendKeys("webdriver.Key.ENTER");
+    //this.browser.sleep(5000);
+    //terminal.sendKeys("uname -a");
+    //terminal.sendKeys(webdriver.Key.ENTER);
+    //this.browser.sleep(5000);
+    //this.browser.window.scrollTo(0, 0);
+    //this.browser.sleep(20000);
+    //var terminal = this.browser.findElement(webdriver.By.id('tty0'));
+    //terminal.click();
+    //terminal.sendKeys('uname -a');
+    //terminal.sendKeys(webdriver.Key.ENTER);
+    this.browser.findElement(webdriver.By.id('tty0')).click();
+    this.browser.sleep(5000);
+    this.browser.sleep(5000);
+    this.browser.sleep(5000);
+    this.browser.sleep(5000);
+    this.browser.findElement(webdriver.By.id('tty0')).sendKeys('uname -a');
+    this.browser.findElement(webdriver.By.id('tty0')).sendKeys(webdriver.Key.ENTER);
+    //this.browser.sendKeys('uname -a');
+    //this.browser.sendKeys(webdriver.Key.ENTER);
+    this.browser.sleep(3000);
+    this.browser.findElement(webdriver.By.id('tty0')).getText().then(function(txt) {
+      assert.equal(txt, "Terminal uses canvas");
+      done();
+    });
+  });
+
+});
+
